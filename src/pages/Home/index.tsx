cd87e4161e0bd8f6a0382c4a70a07bbcc10c9bb0
Gabriel Rosa <gabrieleduardo.s.rosa@gmail.com>
2023-07-29 14:22:27 -0300

    #feat- limpa react-app e inicia rotas

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+    return (
+        <p>home</p>
+    );
+}
+
+export default Home;
---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+
+
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home';
+// import { Container } from './styles';
+
+const RoutesApp: React.FC = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<Home />} />
+        </Routes>
+    );
+}
+
+export default RoutesApp;
