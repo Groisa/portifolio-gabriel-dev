27a417d7a39c1dec92748a59bb8ceaa3f0c89d06
Gabriel Rosa <gabrieleduardo.s.rosa@gmail.com>
2023-08-02 00:02:14 -0300

    FEAT- nova animação da homo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from 'react';
-import { Homemain } from './styles';
+import React, { useEffect, useState } from 'react';
+import { AnimationEarthState, AnimationMoonState, EarthImage, Homemain, ImageMoon } from './styles';
 import Rocket from '../../assets/images/rocket.png'
+import Moon from '../../assets/images/lua.png'
+import Earth from '../../assets/images/terra.png'
 // import { Container } from './styles';
 import './index.css'
 const Home: React.FC = () => {
+
+    const [animationState, setAnimationState] = useState<AnimationMoonState>({ state: undefined })
+    const [animationEarth, setAnimationEarth] = useState<AnimationEarthState>({ state: undefined })
     const stars = () => {
         let count = 50;
         let cenaRocket = document.querySelector('.cenaRocket');
@@ -33,19 +38,64 @@ const Home: React.FC = () => {
                 }
                 i++
             }
-        }, 2250)
+        }, 1500)
+
+
     }
 
     useEffect(() => {
-        stars();
+
     }, [])
+    const handleInitProject = () => {
+
+        stars();
+        setAnimationState({ state: 'init' })
+        setTimeout(() => {
+            setAnimationState({ state: 'complete' })
+        }, 2000)
+        setAnimationEarth({ state: 'start' })
+
+    }
+
+    const StartComponent: React.FC = () => {
+        return (
+            <div className='container'
+                onClick={() => handleInitProject()}
+            >
+                <div className="coast">
+                    <div className="wave-rel-wrap">
+                        <div className="wave"></div>
+                    </div>
+                </div>
+                <div className="coast delay">
+                    <div className="wave-rel-wrap">
+                        <div className="wave delay"></div>
+                    </div>
+                </div>
+                <div className="text text-w">S</div>
+                <div className="text text-a">T</div>
+                <div className="text text-v">A</div>
+                <div className="text text-e">R</div>
+                <div className="text text-t">T</div>
+            </div>
+        )
+    }
+
+
     return (
         <Homemain>
-            <div className="cenaRocket absolut" >
+            <ImageMoon state={animationState.state} src={`${Moon}`} alt="imagem de lua" />
+            {
+                animationState.state !== 'complete' && animationState.state !== 'init' && (
+                    <StartComponent />
+                )
+            }
+            <div className="cenaRocket" >
                 <div className='rocket'>
                     <img src={`${Rocket}`} alt="imagem de foquete" />
                 </div>
             </div>
+            <EarthImage src={`${Earth}`} state={animationEarth.state} />
         </Homemain>
     );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -8,4 +8,42 @@ export const Homemain = styled.main`
     justify-content: center;
     align-items: center;
     display: flex;
+    overflow: hidden !important;
+`
+
+export interface AnimationMoonState {
+    state: 'init' | 'complete' | undefined;
+}
+export interface AnimationEarthState {
+    state: 'start' | undefined;
+}
+
+
+
+export const ImageMoon = styled.img<AnimationMoonState>`
+${({ state }) => state === 'init' && `
+   top: 80px !important;
+   
+` }
+    transition: 4s;
+    position: fixed;
+    top: -8000px;
+    width: 180px;
+    left: 25%;
+${({ state }) => state === 'complete' && `
+    top: 800px !important;
+    
+` }    
+`
+export const EarthImage = styled.img<AnimationEarthState>`
+    width: 40%;
+    position: fixed;
+    left: 30%;
+    top: 48%;
+    transition: 2s;
+    transform: rotate(32deg);
+    ${({state}) => state === 'start' && `
+        top: 800px;
+    `}
+    z-index: 10;
 `
